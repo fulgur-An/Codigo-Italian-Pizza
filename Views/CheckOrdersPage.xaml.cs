662e14d89d135a6cf3aca4458d83b10d38e74c89
Antonio Dominguez <antdguez77@gmail.com>
2022-04-12 14:09:48 -0500

    Primer commit del proyecto

---
+++ Views/CheckOrdersPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItalianPizza.Views
+{
+    /// <summary>
+    /// Lógica de interacción para CheckOrdersPage.xaml
+    /// </summary>
+    public partial class CheckOrdersPage : Page
+    {
+        private string usernameLoggedIn;
+        public CheckOrdersPage(string usernameLoggedIn)
+        {
+            InitializeComponent();
+            this.usernameLoggedIn = usernameLoggedIn;
+        }
+
+        #region GUI Methods
+
+        public void ShowSearchResults(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                InitialMessageBorder.Visibility = Visibility.Hidden;
+                OrderTableGrid.Visibility = Visibility.Visible;
+            }
+        }
+
+        public void ShowFilters(object sender, RoutedEventArgs e)
+        {
+            if (!FirstLayerFilterBorder.IsVisible)
+            {
+                FiltersStackPanel.Visibility = Visibility.Visible;
+                FirstLayerFilterBorder.Visibility = Visibility.Visible;
+                SecondLayerFilterBorder.Visibility = Visibility.Visible;
+            } else
+            {
+                FiltersStackPanel.Visibility = Visibility.Hidden;
+                FirstLayerFilterBorder.Visibility = Visibility.Hidden;
+                SecondLayerFilterBorder.Visibility = Visibility.Hidden;
+            }
+        }
+
+        public void HideFilters(object sender, RoutedEventArgs e)
+        {
+            if (SecondLayerFilterBorder.IsVisible)
+            {
+                FiltersStackPanel.Visibility = Visibility.Hidden;
+                FirstLayerFilterBorder.Visibility = Visibility.Hidden;
+                SecondLayerFilterBorder.Visibility = Visibility.Hidden;
+            }
+        }
+
+        public void ShowSpecificOrderInformation(object sender, RoutedEventArgs e)
+        {
+            ThirdLayerInformationBorder.Visibility = Visibility.Visible;
+            QuarterLayerInformationBorder.Visibility = Visibility.Visible;
+            OrderInformationGrid.Visibility = Visibility.Visible;
+        }
+
+        public void HideSpecificOrderInformation(object sender, RoutedEventArgs e)
+        {
+            ThirdLayerInformationBorder.Visibility = Visibility.Hidden;
+            QuarterLayerInformationBorder.Visibility = Visibility.Hidden;
+            OrderInformationGrid.Visibility = Visibility.Hidden;
+        }
+
+        #endregion
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using ItalianPizza.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItalianPizza
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string usernameLoggedIn;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.usernameLoggedIn = UserLoggedInTextBlock.Text;
+            MainPage mainPage = new MainPage();
+            NavigationFrame.NavigationService.Navigate(mainPage);
+        }
+
+        #region GUI Methods
+
+        public void OpenStartupModule(object sender, RoutedEventArgs e)
+        {
+            MainPage mainPage = new MainPage();
+            NavigationFrame.NavigationService.Navigate(mainPage);
+            OrdersStackPanel.Opacity = 0.5;
+            OrdersGreendBorder.Visibility = Visibility.Hidden;
+        }
+
+        public void OpenCustomerModule(object sender, RoutedEventArgs e) {
+
+        }
+
+        public  void OpenOrderModule(object sender, RoutedEventArgs e)
+        {
+            CheckOrdersPage checkOrdersPage = new CheckOrdersPage(usernameLoggedIn);
+            NavigationFrame.NavigationService.Navigate(checkOrdersPage);
+            OrdersStackPanel.Opacity = 1;
+            OrdersGreendBorder.Visibility = Visibility;
+        }
+
+        #endregion
+    }
+}
