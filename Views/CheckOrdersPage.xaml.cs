f01b4a2c6a988a3c49159b1679c904ad967d41c7
camarillovilla <dukecamarillo@gmail.com>
2022-04-13 20:24:21 -0500

    Layouts of the prototypes of 'Consult order' and 'Register order'

--- Views/CheckOrdersPage.xaml.cs
+++ Views/CheckOrdersPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,13 +22,52 @@ namespace ItalianPizza.Views
     public partial class CheckOrdersPage : Page
     {
         private string usernameLoggedIn;
+
         public CheckOrdersPage(string usernameLoggedIn)
         {
             InitializeComponent();
             this.usernameLoggedIn = usernameLoggedIn;
         }
 
-        #region GUI Methods
+        #region GUI Methods  
+
+        public void ShowOrderDialogs(bool isEnabled)
+        {
+            ThirdLayerInformationBorder.Visibility = Visibility.Visible;
+            QuarterLayerInformationBorder.Visibility = Visibility.Visible;
+            OrderInformationGrid.Visibility = Visibility.Visible;
+            CustomerNameComboBox.IsEnabled = isEnabled;
+            OrderTypeComboBox.IsEnabled = isEnabled;
+            OrderAddressComboBox.IsEnabled = isEnabled;
+        }
+
+        public void ShowOrderRegistrarionDialogue(object sender, RoutedEventArgs e)
+        {
+            ShowOrderDialogs(true);
+            OrderHeaderTextBlock.Text = "Registro de Pedido";
+
+            RegisterCustomerButton.Visibility = Visibility.Visible;
+            AcceptOrderRegistrationButtton.Visibility = Visibility.Visible;
+            UpdateOrderButton.Visibility = Visibility.Collapsed;
+            CancelOrderButton.Visibility = Visibility.Collapsed;
+            GenerateOrderTicketButton.Visibility = Visibility.Collapsed;
+            CustomerNameComboBox.SelectedIndex = -1;
+            OrderTypeComboBox.SelectedIndex = -1;
+            OrderAddressComboBox.SelectedIndex = -1;
+            OrderDateDatePicker.SelectedDate = DateTime.Now;
+            OrderDateTimePicker.SelectedTime = DateTime.Now;
+        }
+
+        public void ShowSpecificOrderInformation(object sender, RoutedEventArgs e)
+        {
+            ShowOrderDialogs(false);
+            OrderHeaderTextBlock.Text = "Pedido";
+            RegisterCustomerButton.Visibility = Visibility.Collapsed;
+            AcceptOrderRegistrationButtton.Visibility = Visibility.Collapsed;
+            UpdateOrderButton.Visibility = Visibility.Visible;
+            CancelOrderButton.Visibility = Visibility.Visible;
+            GenerateOrderTicketButton.Visibility = Visibility.Visible;
+        }
 
         public void ShowSearchResults(object sender, KeyEventArgs e)
         {
@@ -63,18 +103,37 @@ namespace ItalianPizza.Views
             }
         }
 
-        public void ShowSpecificOrderInformation(object sender, RoutedEventArgs e)
+        public void ResetSearchFilters(object sender, RoutedEventArgs e)
         {
-            ThirdLayerInformationBorder.Visibility = Visibility.Visible;
-            QuarterLayerInformationBorder.Visibility = Visibility.Visible;
-            OrderInformationGrid.Visibility = Visibility.Visible;
+            CustomerFilterRadioButton.IsChecked = false;
+            DateFilterRadioButton.IsChecked = false;
+            StatusFilterRadioButton.IsChecked = false;
+            SearchTextBox.Text = "";
+            InitialMessageBorder.Visibility = Visibility.Visible;
+            OrderTableGrid.Visibility = Visibility.Hidden;
         }
 
         public void HideSpecificOrderInformation(object sender, RoutedEventArgs e)
         {
-            ThirdLayerInformationBorder.Visibility = Visibility.Hidden;
-            QuarterLayerInformationBorder.Visibility = Visibility.Hidden;
-            OrderInformationGrid.Visibility = Visibility.Hidden;
+            bool isRegister = OrderHeaderTextBlock.Text.Equals("Registro de Pedido") ? true : false;
+
+            if  (isRegister && !InvalidFieldsGrid.IsVisible)
+            {
+                FifthLayerBorder.Visibility = Visibility.Visible;
+                InvalidFieldsGrid.Visibility = Visibility.Visible;
+            } else
+            {
+                ThirdLayerInformationBorder.Visibility = Visibility.Hidden;
+                QuarterLayerInformationBorder.Visibility = Visibility.Hidden;
+                OrderInformationGrid.Visibility = Visibility.Hidden;
+                BackToOrderRegistration(sender, e);
+            }
+        }
+
+        public void BackToOrderRegistration(object sender, RoutedEventArgs e)
+        {
+            FifthLayerBorder.Visibility = Visibility.Hidden;
+            InvalidFieldsGrid.Visibility = Visibility.Hidden;
         }
 
         #endregion
