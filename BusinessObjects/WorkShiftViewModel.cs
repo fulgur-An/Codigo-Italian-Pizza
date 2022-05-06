2b069c2ac29d10a4296bd6e3f05001b8328f2191
JavierBlas <zs19014035@estudiantes.uv.mx>
2022-05-06 02:00:52 -0500

    dev commit JavierBlas version 2

---
+++ BusinessObjects/WorkShiftViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItalianPizza.BusinessObjects
+{
+    public class WorkShiftViewModel
+    {
+        public string TimeOfEntry { get; set; }
+        public string DepartureTime { get; set; }
+    }
+}
---
+++ Validations/Phone.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace ItalianPizza.Validations
+{
+    public class Phone : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var text = value as string;
+
+            if (text == null)
+            {
+                return new ValidationResult(false, "El campo es obligatorio");
+            }
+
+            string regularPhrase = @"^[0-9]{10}$";
+
+            if (!Regex.IsMatch(text, regularPhrase))
+            {
+                return new ValidationResult(false, "El campo debe contener un número de contacto (10 dígitos)");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
